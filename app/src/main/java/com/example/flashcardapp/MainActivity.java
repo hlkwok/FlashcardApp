34b7d5b3bbeae815cef0039ee0bdfb7ed67461fc
hlkwok <hlkwok@vt.edu>
2021-02-26 13:25:58 -0500

    Added Multiple Choice Answers

--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.flashcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
@@ -26,6 +27,54 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.answer1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer1).setBackgroundColor(getResources().getColor(R.color.wrong_red));
+                findViewById(R.id.answer3).setBackgroundColor(getResources().getColor(R.color.correct_green));
+            }
+        });
+
+        findViewById(R.id.answer2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer2).setBackgroundColor(getResources().getColor(R.color.wrong_red));
+                findViewById(R.id.answer3).setBackgroundColor(getResources().getColor(R.color.correct_green));
+            }
+        });
+
+        findViewById(R.id.answer3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer3).setBackgroundColor(getResources().getColor(R.color.correct_green));
+            }
+        });
+
+        findViewById(R.id.show_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.show_icon).setVisibility(View.INVISIBLE);
+                findViewById(R.id.hide_icon).setVisibility(View.VISIBLE);
+                findViewById(R.id.answer1).setVisibility(View.VISIBLE);
+                findViewById(R.id.answer2).setVisibility(View.VISIBLE);
+                findViewById(R.id.answer3).setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.hide_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.hide_icon).setVisibility(View.INVISIBLE);
+                findViewById(R.id.show_icon).setVisibility(View.VISIBLE);
+                findViewById(R.id.answer1).setVisibility(View.INVISIBLE);
+                findViewById(R.id.answer2).setVisibility(View.INVISIBLE);
+                findViewById(R.id.answer3).setVisibility(View.INVISIBLE);
+                findViewById(R.id.answer1).setBackgroundColor(getResources().getColor(R.color.default_background));
+                findViewById(R.id.answer2).setBackgroundColor(getResources().getColor(R.color.default_background));
+                findViewById(R.id.answer3).setBackgroundColor(getResources().getColor(R.color.default_background));
             }
         });
 
